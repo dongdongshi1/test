cd177d83d91c765f9d1e0e116f58313108d2b7d0
dongsongshi1 <542532236@qq.com>
2017-12-26 17:39:34 +0800

    修改

--- test/src/com/sdd/test/TestDemo.java
+++ test/src/com/sdd/test/TestDemo.java
@@ -2,8 +2,7 @@ package com.sdd.test;
 
 public class TestDemo {
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		
+		System.out.println("hello world ! sdd");
 	}
 
 }
